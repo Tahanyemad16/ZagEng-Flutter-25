f3d44bd92c405cd753f961b451362cc50941c81d
Tahanyemad16 <tahanyemad30@example.com>
2024-12-13 02:17:54 +0200

    Solutions added

---
+++ Task1/problem2/bin/problem2.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  double timeInHours=25/40;
+  double timeInMinutes=(25*60)/40;
+  double timeInseconds=(25*60*60)/40;
+
+  print("Time in hours = $timeInHours"+"   hour");
+  print("Time in Minutes = $timeInMinutes"+"  minutes");
+  print("Time in Seconde = $timeInseconds"+"  seconds");
+
+}
---
+++ Task1/problem3/bin/problem3.dart
@@ -0,0 +1,44 @@
+void main(){
+  var value=10;
+  //print the real value
+  print(value);
+ //decrement from 10 to 9  means value=10-1=9
+  value--;
+  print(value);
+
+  //decrement from 9 to 8  means value=9-1=8
+  value--;
+  print(value);
+
+  //decrement from 8 to 7  means value=8-1=7
+  value--;
+  print(value);
+
+  //decrement from 7 to 6  means value=7-1=6
+  value--;
+  print(value);
+
+  //decrement from 6 to 5  means value=6-1=5
+  value--;
+  print(value);
+
+  //decrement from 5 to 4  means value=5-1=4
+  value--;
+  print(value);
+
+  //decrement from 4 to 3  means value=4-1=3
+  value--;
+  print(value);
+
+  //decrement from 3 to 2  means value=3-1=2
+  value--;
+  print(value);
+
+  //decrement from 2 to 1  means value=2-1=1
+  value--;
+  print(value);
+
+  //decrement from 1 to 0  means value=1-1=0
+  value--;
+  print(value);
+}
